99e88019c0bcc11c5ec58860b4a8821dfe114bf3
tlima1011 <thiagolima1011@gmail.com>
2026-02-07 19:15:02 -0300

    Testes com vetores de aluguel e no main

--- StudentRent/Program.cs
+++ StudentRent/Program.cs
@@ -6,7 +6,7 @@ internal class Program
     static void Main(string[] args)
     {
 
-        int[] rooms = new int[10];
+        Student [] rooms = new Student[10];
 
         Console.Write("Quantos quartos serão alugados? ");
         int n = int.Parse(Console.ReadLine());
@@ -14,6 +14,26 @@ internal class Program
         for (int i = 0; i < n; i++)
         {
             Console.Write("Aluguel #{0}\n", (i+1));
+            Console.Write("Nome: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Quarto: ");
+            int quarto = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            if (rooms[i] == null)
+            {
+                rooms[quarto] = new Student {Name = name, Email = email};
+            }
+        }
+
+        Console.WriteLine("Quartos Ocupados");
+        for(int i = 0; i < 10; i++)
+        {
+            if (rooms[i] != null)
+            {
+                Console.WriteLine("{0}: {1}", i, rooms[i]);
+            }
         }
 
         /*Aluguel #1:
--- StudentRent/Student.cs
+++ StudentRent/Student.cs
@@ -8,5 +8,16 @@ namespace StudentRent
     {
         public String Name { get; set; }
         public String Email { get; set; }
+
+
+
+        public override string ToString()
+        {
+            return Name + ", " + Email;
+        }
     }
+
+    
+
+  
 }
