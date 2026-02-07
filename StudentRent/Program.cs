efc8c2ee4152d3fa0b3934d249bd0ca97bb22836
tlima1011 <thiagolima1011@gmail.com>
2026-02-07 18:43:09 -0300

    Criacao Classe Student

--- StudentRent/Program.cs
+++ StudentRent/Program.cs
@@ -1,9 +1,26 @@
 ﻿using System;
-namespace StudentRent; 
+namespace StudentRent;
+
 internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+
+        int[] rooms = new int[10];
+
+        Console.Write("Quantos quartos serão alugados? ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Aluguel #{0}\n", (i+1));
+        }
+
+        /*Aluguel #1:
+        Nome: Maria Green
+        Email: maria @gmail.com
+        Quarto: 5*/
+
+
     }
 }
---
+++ StudentRent/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentRent
+{
+    internal class Student
+    {
+        public String Name { get; set; }
+        public String Email { get; set; }
+    }
+}
